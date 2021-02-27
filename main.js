08d1fa913282bd8c8242b53a9bc46bc8d56ed203
Dania Rinker <daniarinker@gmail.com>
2021-02-26 20:39:45 -0800

    formulated logic

--- main.js
+++ main.js
@@ -10,58 +10,7 @@ let scoreStart = "0";
 // setInterval(function())
 // let timeclock = 60;
 
-const questions = [
-  {
-    question: "What is JavaScript?",
-    choices: [
-      "The art of writing with coffee",
-      "A programming language",
-      "A scale in which we measure earthquakes",
-      "A fine dine meal originating from France",
-    ],
-    correct: 2,
-  },
-  {
-    question: "When referring to Javascript, what is a 'callback'?",
-    choices: [
-      "What angry people do mid-argument while coding",
-      "A way to attach multiple letters in a string",
-      "A function that's invoked when something happens",
-      "When multiple children elements are called",
-    ],
-    correct: 2,
-  },
-  {
-    question: "When referring to Javascript, what is 'scope'?",
-    choices: [
-      "A visual tool; Explorers use them often to find rogue codes at sea",
-      "An attachment that helps you hunt down your coding bugs",
-      "A variable, in what portion of a program that variable is visible",
-      "When you finally understand what you're doing in VS Code",
-    ],
-    correct: 3,
-  },
-  {
-    question: "When referring to JavaScript, what does 'stateful' mean?",
-    choices: [
-      "When a country is full of them; for example, America",
-      "The opposite of forgetful, especially when coding",
-      "A component, class, or function that manages its own data (state)",
-      "When you become aware of reality; like Neo becomes in the Matrix",
-    ],
-    correct: 3,
-  },
-  {
-    question: "When referring to JavaScript, what is a 'tree shaking'?",
-    choices: [
-      "Removing dead code",
-      "Removing alive code",
-      "When an apple falls on your head and you discover gravity",
-      "When you're trying to get your code to work",
-    ],
-    correct: 1,
-  },
-];
+
 
 function startTimer() {
   var timeClock = 60;
--- quiz.js
+++ quiz.js
@@ -1,5 +1,99 @@
+const questions = [
+  {
+    questionText: "What is JavaScript?",
+    choices: [
+      { text: "The art of writing with coffee" },
+      { text: "A programming language", correct: true },
+      { text: "A scale in which we measure earthquakes" },
+      { text: "A fine dine meal originating from France" },
+    ],
+  },
+  {
+    questionText: "When referring to Javascript, what is a 'callback'?",
+    choices: [
+      { text: "What angry people do mid-argument while coding" },
+      { text: "A way to attach multiple letters in a string" },
+      {
+        text: "A function that's invoked when something happens",
+        correct: true,
+      },
+      { text: "When multiple children elements are called" },
+    ],
+  },
+  {
+    questionText: "When referring to Javascript, what is 'scope'?",
+    choices: [
+      {
+        text:
+          "A visual tool; Explorers use them often to find rogue codes at sea",
+      },
+      { text: "An attachment that helps you hunt down your coding bugs" },
+      {
+        text:
+          "A variable, in what portion of a program that variable is visible",
+        correct: true,
+      },
+      { text: "When you finally understand what you're doing in VS Code" },
+    ],
+  },
+  {
+    questionText: "When referring to JavaScript, what does 'stateful' mean?",
+    choices: [
+      { text: "When a country is full of them; for example, America" },
+      { text: "The opposite of forgetful, especially when coding" },
+      {
+        text:
+          "A component, class, or function that manages its own data (state)",
+        correct: true,
+      },
+      {
+        text:
+          "When you become aware of reality; like Neo becomes in the Matrix",
+      },
+    ],
+  },
+  {
+    questionText: "When referring to JavaScript, what is a 'tree shaking'?",
+    choices: [
+      { text: "Removing dead code", correct: true },
+      { text: "Removing alive code" },
+      { text: "When an apple falls on your head and you discover gravity" },
+      { text: "When you're trying to get your code to work" },
+    ],
+  },
+];
+
+const startButton = document.getElementById("startButton");
+const questionContainer = document.getElementById("questionContainer");
+
+startButton.addEventListener("click", startQuiz);
 
 function startQuiz() {
-    console.log("working!");
-    startButton;
+  startButton.classList.add("hide");
+  questionContainer.classList.remove("hide");
+}
+
+let currentQuestionIndex = 0;
+function nextQuestion() {
+  if (currentQuestionIndex >= questions.length) {
+    renderScore(calculateScore());
+    return;
   }
+
+  // choose the next question
+  renderQuestion(questions[currentQuestionIndex++]);
+}
+
+//foreach answer in question, render button
+function renderQuestion(question) {
+  for (const answer of questions.choices) {
+    // render li button
+  }
+}
+
+//get chosen answer index
+function validateAnswer(index) {}
+
+function calculateScore() {}
+
+function renderScore(scoreInfo) {}
